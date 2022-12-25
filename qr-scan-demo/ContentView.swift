a100a85b49881685e5f095db4e54e2d939d0952f
h-taro <t.hiraishi@bravesoft.co.jp>
2022-12-26 00:23:59 +0900

    Initial Commit

---
+++ qr-scan-demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  qr-scan-demo
+//
+//  Created by 平石　太郎 on 2022/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ qr-scan-demo/qr_scan_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  qr_scan_demoApp.swift
+//  qr-scan-demo
+//
+//  Created by 平石　太郎 on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct qr_scan_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
